961a53f74fe5a11b88e86b4fdd8faa2bdfee5d7b
martinarrose <mmartinarrose@gmail.com>
2026-03-24 18:29:16 +0100

    snack-2

--- main.js
+++ main.js
@@ -43,3 +43,45 @@ for (let i = 1; i < bikes.length; i++) {
   }
 }
 console.log(lighterBike);
+
+//---------------------------------------------------------------------
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+const soccerPlayers = [
+  {
+    name: "argentina",
+    goalScored: 0,
+    foulDrown: 0,
+  },
+  {
+    name: "Italia",
+    goalScored: 0,
+    foulDrown: 0,
+  },
+  {
+    name: "Brasile",
+    goalScored: 0,
+    foulDrown: 0,
+  },
+];
+
+soccerPlayers.forEach((team) => {
+  team.goalScored = Math.floor(Math.random() * 21);
+  team.foulDrown = Math.floor(Math.random() * 21);
+});
+
+console.log(soccerPlayers);
+
+soccerPlayers.map((team) => {
+  return {
+    name: team.name,
+    goalScored: team.goalScored,
+  };
+});
