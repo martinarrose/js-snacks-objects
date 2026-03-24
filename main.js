7df69683a0bd345cbcc93c64b2f8b30d37ddc019
martinarrose <mmartinarrose@gmail.com>
2026-03-24 12:18:48 +0100

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+const bikes = [
+  {
+    name: "City Bike",
+    weight: 20,
+  },
+  {
+    name: "Mountain Bike",
+    weight: 14,
+  },
+  {
+    name: "racing bike",
+    weight: 7.5,
+  },
+  {
+    name: "BMX",
+    weight: 9.5,
+  },
+];
